456abf93141d0a16e6d7fd166d3f25680db177c0
Fran_27 <27francis3@gmail.com>
2025-11-03 19:52:13 -0600

    Apenas voy a probar, pero sospecho que algo anda mal

---
+++ test/calcu.test.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect, } from 'vitest';
+const operaciones = require('../src/calcu');
+
+describe('Pruebas de la calculadora', () => {
+    it('Suma de 2 y 3 debe ser 5', () => {
+    expect(operaciones.sumar(2, 3)).toBe(5);
+    });
+    it('Resta de 5 y 2 debe ser 3', () => {
+    expect(operaciones.restar(5, 2)).toBe(3);
+    });
+
+    it('Multiplicación de 4 y 3 debe ser 12', () => {
+    expect(operaciones.multiplicar(4, 3)).toBe(12);
+    });
+
+    it('División de 10 y 2 debe ser 5', () => {
+    expect(operaciones.dividir(10, 2)).toBe(5);
+    });
+});
